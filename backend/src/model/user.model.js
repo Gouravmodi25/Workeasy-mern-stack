595e8d89990dc1cf5b39332c636d22866acc78c4
Gouravmodi25 <gouravmodi39@gmail.com>
2025-03-01 02:18:42 +0530

    add user model with schema, password hashing, and token generation methods

---
+++ backend/src/model/user.model.js
@@ -0,0 +1,162 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const addressSchema = new mongoose.Schema({
+  landmark: { type: String },
+  city: { type: String },
+  state: { type: String },
+  address: { type: String },
+});
+
+const userSchema = new mongoose.Schema(
+  {
+    // for necessary fields
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    fullname: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      unique: true,
+      sparse: true,
+      required: true,
+    },
+
+    // OTP & Verification
+    otp: {
+      type: String,
+      select: false,
+    },
+    otpExpires: {
+      type: Date,
+      select: false,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    // for profile details
+    mobileNumber: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "Other"],
+    },
+    dob: {
+      type: Date,
+    },
+    avatarImage: {
+      type: String,
+    },
+
+    //   for tokens
+    accessToken: {
+      type: String,
+      select: false,
+    },
+    refreshToken: {
+      type: String,
+      select: false,
+    },
+
+    //   for reset password token
+    resetPasswordToken: {
+      type: String,
+    },
+    resetPasswordTokenExpiry: {
+      type: String,
+    },
+
+    //   for address schema
+    address: addressSchema,
+  },
+  {
+    timestamps: true, // Automatically add createdAt & updatedAt
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+// for age calculation
+userSchema.virtual("age").get(function () {
+  if (this.dob) {
+    const diff = new Date() - new Date(this.dob);
+    return Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
+  }
+  return null;
+});
+
+// for password hashing
+userSchema.pre("save", async function (next) {
+  if (!this.isModified.password) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// for correct password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// is Same Password
+userSchema.methods.isSamePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// generate access token
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+      username: this.username,
+      email: this.email,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+// generate refresh token
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+const UserModel = mongoose.model("User", userSchema);
+
+module.exports = UserModel;
