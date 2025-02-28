529528144dcb823e82f190ac351d1d6cd635af2b
Gouravmodi25 <gouravmodi39@gmail.com>
2025-03-01 00:52:16 +0530

    add ApiResponse utility class for standardized API responses

---
+++ backend/src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+class ApiResponse {
+  constructor(statusCode, message = "success", data = null) {
+    this.statusCode = statusCode;
+    this.message = message;
+    this.success = statusCode >= 200 && statusCode < 400;
+    this.data = data;
+  }
+}
+
+MediaSourceHandle.exports = ApiResponse;
