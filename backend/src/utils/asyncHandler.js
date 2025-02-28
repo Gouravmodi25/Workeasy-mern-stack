2959e04a5b9980902b8e6582f7415d6e57d779b7
Gouravmodi25 <gouravmodi39@gmail.com>
2025-03-01 00:23:24 +0530

    add utils for handler async operation

---
+++ backend/src/utils/asyncHandler.js
@@ -0,0 +1,9 @@
+const asyncHandler = (handlerFunction) => {
+  return async (req, res, next) => {
+    return Promise.resolve(handlerFunction(req, res, next)).catch((error) => {
+      return next(error);
+    });
+  };
+};
+
+module.export = asyncHandler;
