316ec26c2055813f921ec9e505872af148249443
Gouravmodi25 <gouravmodi39@gmail.com>
2025-03-01 00:52:06 +0530

    add multer middleware for file uploads and update package.json

---
+++ backend/src/middleware/multer.middleware.js
@@ -0,0 +1,20 @@
+const multer = require("multer");
+
+// Multer configuration for upload file
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/temp");
+  },
+  filename: (req, file, cb) => {
+    const fileName = `${file.originalname}-${Date.now()}`;
+    cb(null, fileName);
+  },
+});
+
+// config the storage and limit of file size only 20MB
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 20 * 1024 * 1024 },
+});
+
+module.exports = upload;
