8a6f7b25fa6d7f3f1d3ea4910f4285a742e25db3
Gouravmodi25 <gouravmodi39@gmail.com>
2025-03-03 13:57:28 +0530

    add utility function to send emails using nodemailer and sendgrid

---
+++ backend/src/utils/sendEmail.js
@@ -0,0 +1,30 @@
+const nodeMailer = require("nodemailer");
+
+// sendMail function
+
+const sendMail = async function (option) {
+  const transporter = nodeMailer.createTransport({
+    service: process.env.EMAIL_SERVICE,
+    auth: {
+      user: process.env.EMAIL_NAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_FROM,
+    to: option.to,
+    subject: option.subject,
+    text: option.text,
+  };
+
+  await transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+};
+
+module.exports = sendMail;
