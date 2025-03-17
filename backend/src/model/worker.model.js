5bc094e06b0c4ee2a78c842670e187663a5cc6ce
Gouravmodi25 <gouravmodi39@gmail.com>
2025-03-17 19:38:12 +0530

    chore://add worker model with fields for authentication, profile details, and password reset functionality

---
+++ backend/src/model/worker.model.js
@@ -0,0 +1,205 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+// address schema
+const addressSchema = new mongoose.Schema({
+  landmark: { type: String },
+  city: { type: String },
+  state: { type: String },
+  address: { type: String },
+  country: { type: String },
+});
+
+const workerSchema = new Schema({
+  // necessary fields
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  fullname: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  phoneNumber: {
+    type: String,
+    unique: true,
+    sparse: true,
+    required: true,
+  },
+
+  //   otp verification
+
+  otp: {
+    type: String,
+    select: false,
+  },
+  otpExpires: {
+    type: Date,
+    select: false,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+
+  //   for profile details
+
+  gender: {
+    type: String,
+    enum: ["Male", "Female", "Transgender", "Other"],
+  },
+  dob: {
+    type: Date,
+  },
+  avatarImage: {
+    type: String,
+  },
+  skill: {
+    type: String,
+  },
+  experience: {
+    type: Number,
+    default: 0,
+  },
+  charges: {
+    type: Number,
+  },
+  availability: {
+    type: Boolean,
+    default: "Available",
+    enum: ["Available", "Unavailable", "On Leave"],
+  },
+  visitationFees: {
+    type: Number,
+  },
+  workerRating: {
+    type: Number,
+    min: 0, // Minimum rating value (if applicable)
+    max: 5, // Maximum rating value
+    validate: {
+      validator: Number.isFinite,
+      message: "Rating must be a valid number",
+    },
+  },
+
+  //   address
+  address: addressSchema,
+
+  //   for reset password token
+  resetPasswordOtp: {
+    type: String,
+  },
+  resetPasswordOtpExpiry: {
+    type: String,
+  },
+
+  isResetOtpVerified: {
+    type: Boolean,
+  },
+
+  resetPasswordToken: {
+    type: String,
+  },
+  resetPasswordTokenExpiry: {
+    type: String,
+  },
+
+  //   for tokens
+  accessToken: {
+    type: String,
+    select: false,
+  },
+  refreshToken: {
+    type: String,
+    select: false,
+  },
+});
+
+// for age calculation
+workerSchema.virtual("age").get(function () {
+  if (this.dob) {
+    const diff = new Date() - new Date(this.dob);
+    return Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
+  }
+  return null;
+});
+
+// for password hashing
+workerSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// for correct password
+workerSchema.methods.isCorrectPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// is Same Password
+workerSchema.methods.isSamePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// generate access token
+workerSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+      username: this.username,
+      email: this.email,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+// generate reset password token
+
+workerSchema.methods.generateResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.resetPasswordTokenExpiry = Date.now() + 10 * (60 * 1000);
+  return resetToken;
+};
+
+// generate refresh token
+workerSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+// create model of worker
+
+const WorkerModel = mongoose.model("Worker", workerSchema);
+
+module.exports = WorkerModel;
