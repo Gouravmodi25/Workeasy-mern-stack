6f728ec192a8d69bf202d1b8113b3a27e13dfa6e
Gouravmodi25 <gouravmodi39@gmail.com>
2025-03-21 08:20:20 +0530

    feat: add date validation utility to ensure valid booking dates

---
+++ backend/src/utils/dateValidation.js
@@ -0,0 +1,18 @@
+const validator = require("validator");
+const dayjs = require("dayjs");
+
+const dateValidation = (date) => {
+  if (!validator.isDate(date, { format: "YYYY-MM-DD", strictMode: true })) {
+    return "Date must be in YYYY-MM-DD format";
+  }
+
+  if (dayjs(date).isBefore(dayjs().format("YYYY-MM-DD"))) {
+    return "Date must be in future not past";
+  }
+
+  if (dayjs(date).day() === 0) {
+    return "Sunday is not available for booking";
+  }
+};
+
+module.exports = dateValidation;
